487e6e7fa66a836f4809e55aa241e341e20a914e
rsovkovic <rsovkovic@gmail.com>
2025-06-23 10:20:50 +0300

    addition

--- src/task-3.ts
+++ src/task-3.ts
@@ -2,6 +2,11 @@ const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
 
+interface Product {
+  id: number;
+  title: string;
+}
+
 const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
@@ -10,8 +15,3 @@ const products: Product[] = [
 console.log(`Usernames: ${JSON.stringify(usernames)}`);
 console.log(`Ratings: ${JSON.stringify(ratings)}`);
 console.log(`Products: ${JSON.stringify(products)}`);
-
-interface Product {
-  id: number;
-  title: string;
-}
